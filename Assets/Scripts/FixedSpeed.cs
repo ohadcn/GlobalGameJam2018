793cc6e17a9731866da1cca8ae1ce9104c3bb6b5
ohad cohen <ohad.c@amusetech.co>
2018-01-26 20:48:49 +0200

    Animations!!

---
+++ Assets/Scripts/FixedSpeed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedSpeed : MonoBehaviour {
+
+    public Vector3 minSpeed = Vector3.zero;
+    public Vector3 maxSpeed = Vector3.one * 1000;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+}
