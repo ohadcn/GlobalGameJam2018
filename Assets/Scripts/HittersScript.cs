8a5e11d6f8c2cd65706b0f300c05f1016a41d9b8
stigersh <stigersh@gmail.com>
2018-01-26 21:43:39 +0200

    change images for hitters

--- Assets/Scripts/HittersScript.cs
+++ Assets/Scripts/HittersScript.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class HittersScript : MonoBehaviour
 {
     public Fruit fruit;
+    public Sprite[] sprites;
 
     static int ty = 0;
+
+    string[] imnames = { "banana", "strawberry", "orange" };
     // Use this for initialization
     void Start()
     {
-        fruit = (Fruit)(ty % 3);
+        fruit = (Fruit)(ty % 3);    
+        GetComponent<SpriteRenderer>().sprite = sprites[ty%3];
+       // Debug.Log("", GetComponent<SpriteRenderer>().sprite);
         ty++;
     }
 
