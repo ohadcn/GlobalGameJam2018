a7841f5168362b12da5d12102653b9f92688cf54
chamutalz <chamutalz@gmail.com>
2018-01-27 11:05:59 +0200

    Some UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,45 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public TextMeshProUGUI counter;
+    public int score;
+    public TextMeshProUGUI timer;
+    public int seconds;
+    int seconds0;
+    public TextMeshProUGUI gameOver;
+    public int countDown;
+    float countDown0;
+    int FramePerSec = 25;
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        counter.text = "Score:";
+        timer.text = "120";       
+        gameOver.text = " ";
+        score = 0;
+        seconds0 = 120;
+        countDown0 = (Time.realtimeSinceStartup);       
+    }
+    private void Update()
+    {
+        UpdateTimer();
+        UpdateScore();
+
+    }
+    void UpdateTimer()
+         {
+        countDown = (int)(Time.realtimeSinceStartup - countDown0);
+        seconds = seconds0 - countDown;
+        timer.text = seconds.ToString();
+         }
+
+    void UpdateScore()
+         {
+            counter.text = "Score: " + score;
+         }
+
 }
