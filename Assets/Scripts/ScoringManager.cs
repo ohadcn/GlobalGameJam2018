7a686e98985c430f8dfbdc444f702b7f5303fbdd
chamutalz <chamutalz@gmail.com>
2018-01-27 11:30:35 +0200

    Revert "half solve issue #5"
    This reverts commit 1991ad16b65a62fcbc56daf837968eea21cdade3.

---
+++ Assets/Scripts/ScoringManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringManager : MonoBehaviour {
+
+    public static int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+        string text = "Score: " + score;
+        GUI.Label(rect, text, style);
+
+    }
+}
